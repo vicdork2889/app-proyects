e5c69373379118b6f3fb09b0e1c017fb676f530c
vicdork2889 <vrmaos2889@gmail.com>
2025-04-28 22:31:07 -0600

    ejemplo

--- proyecto tareas/gui.py
+++ proyecto tareas/gui.py
@@ -1,9 +1,13 @@
 from funciones import get_todos,write_todos
 import FreeSimpleGUI as sg
+import time
 
+sg.theme("DarkPurple3")
+
+reloj = sg.Text('',key='reloj')
 label = sg.Text("Escribe una tarea")
 input_box = sg.InputText(tooltip="Enter todo", key="tarea")
-boton_añadir = sg.Button("Añadir")
+boton_añadir = sg.Button("Añadir",size=12)
 impresion_lista = sg.Listbox(values=get_todos(),key=('tareas'),
                              enable_events =True, size=[45,10])
 boton_editar = sg.Button("editar")
@@ -12,16 +16,15 @@ boton_salida = sg.Button("salida")
 
 
 putamadre = sg.Window('Lista de tareas',
-                      layout =[[label],[input_box,boton_añadir],
+                      layout =[[reloj],
+                          [label],[input_box,boton_añadir],
                                [impresion_lista,boton_editar, boton_completar],
                                [boton_salida]],
                       font =('Helvetica',20))
 
 while True:
-    momento,valores = putamadre.read()
-    print(1,momento)
-    print(2,valores)
-    print(3,valores['tarea'])
+    momento,valores = putamadre.read(timeout=10)
+    putamadre["reloj"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
     match momento:
         case "Añadir":
             tarea = get_todos()
@@ -31,21 +34,27 @@ while True:
             putamadre["tareas"].update(values=tarea)
 
         case "editar":
-            tarea_editada = valores['tareas'][0]
-            nueva_tarea = valores['tarea']+'\n'
-            tareas = get_todos()
-            index = tareas.index(tarea_editada)
-            tareas[index] = nueva_tarea
-            write_todos(tareas)
-            putamadre["tareas"].update(values=tareas)
+            try:
+                tarea_editada = valores['tareas'][0]
+                nueva_tarea = valores['tarea']+'\n'
+                tareas = get_todos()
+                index = tareas.index(tarea_editada)
+                tareas[index] = nueva_tarea
+                write_todos(tareas)
+                putamadre["tareas"].update(values=tareas)
+            except IndexError:
+                sg.popup("porfavor primero elige una tarea", font=("Helvetica",16))
 
         case "completada":
-            tareas_a_completar = valores['tareas'][0]
-            tareas = get_todos()
-            tareas.remove(tareas_a_completar)
-            write_todos(tareas)
-            putamadre["tareas"].update(values=tareas)
-            putamadre["tarea"].update(value='')
+            try:
+                tareas_a_completar = valores['tareas'][0]
+                tareas = get_todos()
+                tareas.remove(tareas_a_completar)
+                write_todos(tareas)
+                putamadre["tareas"].update(values=tareas)
+                putamadre["tarea"].update(value='')
+            except IndexError:
+                sg.popup("porfavor primero elige una tarea", font=("Helvetica",16))
 
         case "salida":
             break
